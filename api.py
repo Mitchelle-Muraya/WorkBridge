bec15dc25ab9c3dad7d98bf4cc64f1da0ba1d6ed
MM-cmd-ui <mitchelle.muraya@strathmore.edu>
2025-11-16 12:11:47 +0300

    deploy

--- api.py
+++ api.py
@@ -93,3 +93,9 @@ def predict_job():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
